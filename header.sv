53450ec4d84c497469b36f656e6200a183881b76
Matteo Cericola <128947575+matteocer@users.noreply.github.com>
2026-03-25 17:49:33 -0400

    Add controller instantiation in header.sv

---
+++ header.sv
@@ -0,0 +1,18 @@
+controller ctrl (
+ 17         .CoinValue(CoinValue),
+ 16         .CoinInserted(CoinInserted),
+ 15         .LoadShape(LoadShape),
+ 14         .ShapeLocation(ShapeLocation),
+ 13         .LoadShapeNow(LoadShapeNow),
+ 12         .StartGame(StartGame),
+ 11         .GradeIt(GradeIt),
+ 10         .Guess(Guess),
+  9         .NumGames(NumGames),
+  8         .RoundNumber(RoundNumber),
+  7         .GameWon(GameWon),
+  6         .Zood(Zood),         .Znarly(Znarly),
+        .loadZnarlyZood(loadZnarlyZood),
+        .clearGame(clearGame),
+          .loadNumGames(loadNumGames),
+         .loadGuess(loadGuess)
+      );
