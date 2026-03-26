498d9d4d36a5f6dd157c7d175696972943d166a4
Matteo Cericola <128947575+matteocer@users.noreply.github.com>
2026-03-26 19:17:06 -0400

    Add files via upload

--- controller.sv
+++ controller.sv
@@ -1,4 +1,8 @@
-`default_nettype none
+//`default_nettype none
+
+/* This is the main controller for this system. It contains
+   an fsm, and complete datapath for the game. */
+
 module controller (
     input logic clock, reset,
     input logic [1:0] CoinValue,
@@ -15,18 +19,25 @@ module controller (
     output logic [3:0] Znarly,
     output logic loadZnarlyZood, loadGuess,
     output logic [2:0] Money,
-    output logic [11:0] masterPattern, output logic BoughtGame);
+    output logic [11:0] masterPattern,
+    output logic BoughtGame,
+    output logic [1:0] State,
+    output logic Loaded1,
+    output logic Loaded2,
+    output logic Loaded3,
+    output logic Loaded0,
+    output logic sLoadShapeOut
+    );
 
     logic [11:0] masterPatternIn;
     logic [2:0] adderA, shape, adderOut, subOut;
     logic [2:0] moneyIn;
-    logic [1:0] sLoc;
     logic GameOver;
 
     logic sStartGame, sLoadShape, sGradeIt, sCoinIn;
     //logic Won;
     //logic BoughtGame;
-    logic sLoadShapeNow;
+
 
 
 
@@ -37,96 +48,176 @@ module controller (
 
     logic grst;
     logic enough, MaxGames;
+    logic clear;
 
     assign shape = LoadShape;
-    assign sLoc = ShapeLocation;
+    assign clearGame = sStartGame;
+    
+    
+    
+   
+   assign sLoadShapeOut = (load_shape_state == 2'b01);
+   assign loadGuess = loadZnarlyZood;
 
-    enum logic [1:0] {START, LOAD, PLAY} current_state, next_state;
+    enum logic [1:0] {INIT, START, LOAD, PLAY} current_state, next_state;
+    assign State = current_state;
+
+    logic [1:0] coin_state, coin_next, load_shape_state, load_shape_next,
+                grade_state, grade_next;
 
     logic pos3en, pos2en, pos1en, pos0en, loaded3, loaded2, loaded1, loaded0,
           pos3clr, pos2clr, pos1clr, pos0clr;
+
+    logic gradeEn;
+
+    assign Loaded0 = loaded0;
+    assign Loaded1 = loaded1;
+    assign Loaded2 = loaded2;
+    assign Loaded3 = loaded3;
+
+    
+//   assign loadGuess = (grade_state == 2'b01) && (current_state == PLAY);
+
+
     always_comb begin
         case (current_state)
-            START: next_state = (enough)? LOAD : START;
-            LOAD: next_state = (sStartGame) ? PLAY : LOAD;
-            PLAY:  next_state = (GameOver | GameWon)? START : PLAY;
+            INIT:  next_state = START;
+            START: next_state = (enough) ? LOAD : START;
+            LOAD:  next_state = (sStartGame & loaded0 & loaded1 & loaded2 & loaded3) 
+                                ? PLAY : LOAD;
+            PLAY:  next_state = (GameOver | GameWon) ? START : PLAY;
         endcase
+
+        case (coin_state)
+            2'b00: coin_next = (sCoinIn) ? 2'b01 : 2'b00;
+            2'b01: coin_next =  2'b11;
+            2'b11: coin_next = (~sCoinIn) ? 2'b10 : 2'b11;
+            2'b10: coin_next = 2'b00;
+        endcase
+        case (load_shape_state)
+            2'b00: load_shape_next = (sLoadShape) ? 2'b01 : 2'b00;
+            2'b01: load_shape_next =  2'b11;
+            2'b11: load_shape_next = (~sLoadShape) ? 2'b10 : 2'b11;
+            2'b10: load_shape_next = 2'b00;
+        endcase
+        case (grade_state)
+            2'b00: grade_next = (sGradeIt) ? 2'b01 : 2'b00;
+            2'b01: grade_next =  2'b11;
+            2'b11: grade_next = (~sGradeIt) ? 2'b10 : 2'b11;
+            2'b10: grade_next = 2'b00;
+        endcase
+
     end
 
     always_ff @(posedge clock, posedge reset) begin
-        Gen  <= 1'b0;
-        Gclr <= 1'b0;
-        Ren  <= 1'b0;
-        Rclr <= 1'b0;
-        Cen  <= 1'b0;
-        Cclr <= 1'b0;
-        Sen  <= 1'b0;
-        Sclr <= 1'b0;
-        grst <= 1'b0;
-        pos3clr <= 1'b0;
-        pos2clr <= 1'b0;
-        pos1clr <= 1'b0;
-        pos0clr <= 1'b0;
-        if (reset) begin
-            current_state <= START;
-            Cclr          <= 1'b1;
-            Gclr          <= 1'b1;
-            Rclr          <= 1'b1;
-            Sclr          <= 1'b1;
-            grst          <= 1'b1;
-            pos3clr <= 1'b1;
-            pos2clr <= 1'b1;
-            pos1clr <= 1'b1;
-            pos0clr <= 1'b1;
-        end else begin
+        if (reset)
+            current_state <= INIT;
+        else
             current_state <= next_state;
+    end
 
-            case (current_state)
-                START: begin
-                    if (BoughtGame & ~MaxGames) begin
-                        Gup <= 1'b1;
-                        Gen <= 1'b1;
-                    end
-                    if (sCoinIn)
-                        Cen <= 1'b1;
-                end
-                LOAD: begin
-                    if (BoughtGame & ~MaxGames) begin
-                        Gen <= 1'b1;
-                        Gup <= 1'b1;
-                    end
-                    if (sLoadShapeNow & sLoc[0] & sLoc[1] & ~loaded3) begin
-                        pos3en <= 1;
-                        Sen <= 1'b1;
-                    end
-                    if (sLoadShapeNow & ~sLoc[0] & sLoc[1] & ~loaded2) begin
-                        pos2en <= 1;
-                        Sen <= 1'b1;
-                    end
-                    if (sLoadShapeNow & sLoc[0] & ~sLoc[1] & ~loaded1) begin
-                        pos1en <= 1;
-                        Sen <= 1'b1;
-                    end
-                    if (sLoadShapeNow & ~sLoc[0] & ~sLoc[1] & ~loaded0) begin
-                        pos0en <= 1;
-                        Sen <= 1'b1;
-                    end
+    always_ff @(posedge clock) begin
+        if (reset)
+            coin_state <= 2'b00;
+        else
+            coin_state <= coin_next;
+    end
+    
+    always_ff @(posedge clock) begin
+        if (reset)
+            load_shape_state <= 2'b00;
+        else
+           load_shape_state <= load_shape_next;
+    end
+    
+    always_ff @(posedge clock) begin
+        if (reset)
+            grade_state <= 2'b00;
+        else
+           grade_state <= grade_next;
+    end
 
-                    if (sCoinIn)
-                        Cen <= 1'b1;
-                    if (sStartGame) begin
-                        Gen <= 1'b1;
-                        Gup <= 1'b0;
-                    end
+
+    always_comb begin
+        Gen  = 1'b0;
+        Gclr = 1'b0;
+        Ren  = 1'b0;
+        Rclr = 1'b0;
+        Cen  = 1'b0;
+        Cclr = 1'b0;
+        Sen  = 1'b0;
+        Sclr = 1'b0;
+        grst = 1'b0;
+        pos3clr = 1'b0;
+        pos2clr = 1'b0;
+        pos1clr = 1'b0;
+        pos0clr = 1'b0;
+        pos3en = 1'b0;
+        pos2en = 1'b0;
+        pos1en = 1'b0;
+        pos0en = 1'b0;
+        gradeEn = 1'b0;
+        Gup = 1'b1;
+        
+        case (current_state)
+            INIT: begin
+                Cclr = 1'b1;
+                Gclr = 1'b1;
+                Rclr = 1'b1;
+                Sclr = 1'b1;
+                grst = 1'b1;
+                pos3clr = 1'b1;
+                pos2clr = 1'b1;
+                pos1clr = 1'b1;
+                pos0clr = 1'b1;
+            end
+            START: begin
+                if (BoughtGame & ~MaxGames) begin
+                    Gen = 1'b1;
                 end
-                PLAY: begin
-                    if (GameOver | GameWon) begin
-                        Rclr <= 1'b1;
-                        Gclr <= 1'b1;
-                    end
+                if ((CoinValue != 2'b00 && coin_state == 2'b01) ||
+                    BoughtGame)
+                    Cen = 1'b1;
+            end
+            LOAD: begin
+                if (BoughtGame & ~MaxGames) begin
+                    Gen = 1'b1;
+                end 
+                if (sLoadShape & ShapeLocation[0] & ShapeLocation[1] & ~loaded3) begin
+                    pos3en = 1;
+                    Sen = 1'b1;
+                end if (sLoadShape & ~ShapeLocation[0] & ShapeLocation[1] & ~loaded2) begin
+                    pos2en = 1;
+                    Sen = 1'b1;
+                end if (sLoadShape & ShapeLocation[0] & ~ShapeLocation[1] & ~loaded1) begin
+                    pos1en = 1;
+                    Sen = 1'b1;
                 end
-            endcase
-        end
+                if (sLoadShape & ~ShapeLocation[0] & ~ShapeLocation[1] & ~loaded0) begin
+                    pos0en = 1;
+                    Sen = 1'b1;
+                end
+  
+
+                if ((CoinValue != 2'b00 && coin_state == 2'b01) ||
+                    BoughtGame)
+                    Cen = 1'b1;
+                if (sStartGame && loaded0 && loaded1 && loaded2 && loaded3) begin
+                    Gen = 1'b1;
+                    Gup = 1'b0;
+                end
+            end
+            PLAY: begin
+                if (GameOver | GameWon) begin
+                    Rclr = 1'b1;
+                    Gclr = 1'b1;
+                end
+                if (grade_state == 2'b01) begin
+                    gradeEn = 1'b1;
+                    Ren = 1'b1;
+                end
+            end
+        endcase
     end
 
     Synchronizer syncCoin (
@@ -138,7 +229,7 @@ module controller (
     Synchronizer syncLS (
         .async(LoadShapeNow),
         .clock(clock),
-        .sync(sLoadShapeNow)
+        .sync(sLoadShape)
     );
 
     Synchronizer syncGI (
@@ -211,10 +302,45 @@ module controller (
         .Q(NumGames)
     );
 
-    Counter #(1) p3count(pos3en, pos3clr, 1'b0, 1'b1, clock, 1'b0, loaded3),
-                 p2count(pos2en, pos2clr, 1'b0, 1'b1, clock, 1'b0, loaded2),
-                 p1count(pos1en, pos1clr, 1'b0, 1'b1, clock, 1'b0, loaded1),
-                 p0count(pos0en, pos0clr, 1'b0, 1'b1, clock, 1'b0, loaded0);
+    Counter #(1) p3count(
+          .en(pos3en), 
+          .clear(pos3clr), 
+          .load(1'b0),
+          .up(1'b1), 
+          .clock(clock), 
+          .D(1'b0), 
+          .Q(loaded3)
+     );
+     
+     Counter #(1) p2count(
+          .en(pos2en), 
+          .clear(pos2clr), 
+          .load(1'b0),
+          .up(1'b1), 
+          .clock(clock), 
+          .D(1'b0), 
+          .Q(loaded2)
+     );
+     
+     Counter #(1) p1count(
+          .en(pos1en), 
+          .clear(pos1clr), 
+          .load(1'b0),
+          .up(1'b1), 
+          .clock(clock), 
+          .D(1'b0), 
+          .Q(loaded1)
+     );
+     
+     Counter #(1) p0count(
+          .en(pos0en), 
+          .clear(pos0clr), 
+          .load(1'b0),
+          .up(1'b1), 
+          .clock(clock), 
+          .D(1'b0), 
+          .Q(loaded0)
+     );
 
     MagComp #(4) compEnough (
         .A(NumGames),
@@ -226,7 +352,7 @@ module controller (
 
     MagComp #(4) compMax (
         .A(NumGames),
-        .B(4'd7),
+        .B(4'd6),
         .AltB(),
         .AeqB(),
         .AgtB(MaxGames)
@@ -251,28 +377,28 @@ module controller (
     Mux2to1 #(3) muxPat3 (
         .I1(shape),
         .I0(masterPattern[11:9]),
-        .S((sLoc[1]&sLoc[0])),
+        .S((ShapeLocation[1]&ShapeLocation[0])),
         .Y(masterPatternIn[11:9])
     );
 
     Mux2to1 #(3) muxPat2 (
         .I1(shape),
         .I0(masterPattern[8:6]),
-        .S((sLoc[1]&~sLoc[0])),
+        .S((ShapeLocation[1]&~ShapeLocation[0])),
         .Y(masterPatternIn[8:6])
     );
 
     Mux2to1 #(3) muxPat1 (
         .I1(shape),
         .I0(masterPattern[5:3]),
-        .S((~sLoc[1]&sLoc[0])),
+        .S((~ShapeLocation[1]&ShapeLocation[0])),
         .Y(masterPatternIn[5:3])
     );
 
     Mux2to1 #(3) muxPat0 (
         .I1(shape),
         .I0(masterPattern[2:0]),
-        .S((~sLoc[1]&~sLoc[0])),
+        .S((~ShapeLocation[1]&~ShapeLocation[0])),
         .Y(masterPatternIn[2:0])
     );
 
@@ -287,9 +413,9 @@ module controller (
     grader grad (
         .CLOCK_100(clock),
         .reset(grst),
-        .masterPattern(masterPattern),
+        .masterPattern(masterPattgradeEnern),
         .guess(Guess),
-        .GradeIt(sGradeIt),
+        .GradeIt(gradeEn),
         .done(loadZnarlyZood),
         .Zood(Zood),
         .Znarly(Znarly)
